f68827d23d64b215cfc21541cd1c52ae1ab43976
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-05-07 22:32:46 -0700

    don't add data.go itself to bindata
    See #2

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
-//go:generate go-bindata -pkg=data -o data.go . ./benkeen/d3pie/ ./jiahuang/d3-timeline/ ./krisk/fuse/ ./zeroclipboard/zeroclipboard/
+//go:generate go-bindata -pkg=data -ignore=.*\.go -o data.go . ./benkeen/d3pie/ ./jiahuang/d3-timeline/ ./krisk/fuse/ ./zeroclipboard/zeroclipboard/
+//go:generate go fmt .
 
 package data
