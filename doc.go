efa2841cf2704832e92b58a87c79bf832f7f2115
Dmitri Shuralyov <shurcooL@gmail.com>
2015-09-21 13:53:20 -0700

    Use correct name of the product in docs.

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package data contains binary and external asset files for appdash.
+// Package data contains binary and external asset files for Appdash.
 package data
 
 //go:generate go run generate.go
