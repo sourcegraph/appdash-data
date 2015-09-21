5f0deb090f6dc925cc4bcecd6f150f12f1fc1bf0
Dmitri Shuralyov <shurcooL@gmail.com>
2015-09-20 22:48:29 -0700

    Use vfsgen for statically embedding http.FileSystem.
    Replace use of go-bindata and go-bindata-assetfs in importer package
    with a single vfsgen package that generates an http.FileSystem
    implementation more directly.
    
    Fix #4 while touching relevant code.

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 This repository holds binary and external asset files (images, CSS, JS etc) that [Appdash](https://sourcegraph.com/sourcegraph/appdash) relies on.
 
-The data is embedded into the final Appdash binary using [go-bindata](https://github.com/jteeuwen/go-bindata) and served via HTTP using [go-bindata-assetfs](https://github.com/elazarl/go-bindata-assetfs).
+The data is embedded into the final Appdash binary using [vfsgen](https://github.com/shurcooL/vfsgen) and served via HTTP using [http.FileServer](https://godoc.org/net/http#FileServer).
 
 # Regenerating
 
-Simply run `go generate` in this directory to regenerate the `data.go` file using [go-bindata](https://github.com/jteeuwen/go-bindata).
+Simply run `go generate` in this directory to regenerate the `data.go` file using [vfsgen](https://github.com/shurcooL/vfsgen).
 
 # Licenses
 
--- generate.go
+++ generate.go
@@ -1,4 +1,38 @@
-//go:generate go-bindata -pkg=data -ignore=.*\.go -o data.go . ./benkeen/d3pie/ ./jiahuang/d3-timeline/ ./krisk/fuse/ ./zeroclipboard/zeroclipboard/ ./seiyria/bootstrap-slider/
-//go:generate go fmt .
+// +build generate
 
-package data
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	pathpkg "path"
+
+	"github.com/shurcooL/go/vfs/httpfs/filter"
+	"github.com/shurcooL/vfsgen"
+)
+
+func main() {
+	ignore := func(fi os.FileInfo, path string) bool {
+		switch {
+		case len(fi.Name()) >= 2 && fi.Name()[0] == '.': // Skip .files/.folders (but not ".").
+			return true
+		case pathpkg.Ext(fi.Name()) == ".go":
+			return true
+		case path == "/README.md":
+			return true
+		default:
+			return false
+		}
+	}
+	var data http.FileSystem = filter.NewIgnore(http.Dir("."), ignore)
+
+	err := vfsgen.Generate(data, vfsgen.Options{
+		Filename:     "data.go",
+		PackageName:  "data",
+		VariableName: "Assets",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
